0da0b84ff05f6a8248540f0331a154147eacc2ff
Rishik <rishikgoyal00@gmail.com>
2021-02-04 01:54:55 +0530

    oauth facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://localhost:27017/database'
+	'mongoUrl': 'mongodb://localhost:27017/database',
+	'facebook': {
+		clientId: '1000664113794722',
+		clientSecret: 'cefaa9a8c891f1013020d058ab73f7aa'
+	}
 }
